5831275e94b887935fdf833aabfd37068dec311c
petri-98 <gtringali4@gmail.com>
2023-06-25 03:25:20 +0200

    feat(point.py): added new method + minor adjustments

--- point.py
+++ point.py
@@ -22,17 +22,20 @@ class Point:
 
     def l2_norm(self):
         return math.hypot(self.x_norm(), self.y_norm(), self.z_norm())
-    
+
+    def cyclic_transpose(self):
+        self.x, self.y, self.z = self.y, self.z, self.x
+
 
 if __name__ == "__main__":
-    point1 = Point(1,2,3)
-    point2 = Point(2,3,4)
+    point1 = Point(1, 2, 3)
+    point2 = Point(2, 3, 4)
 
     print(point1)
     print(point2)
-    
+
     print(point1.l1_norm())
     print(point1.l2_norm())
-
-
-
+    
+    point1.cyclic_transpose()
+    print(point1)
